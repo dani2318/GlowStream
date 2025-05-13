43cdf33d9be501715e9080b1296506e460f1bafe
dani2318 <giacche.dani@gmail.com>
2025-05-13 12:54:00 +0200

    Started integrating twitch analytics api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,53 @@
 "use client";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { LogOut } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function Home() {
   const { data: session } = useSession();
 
+  useEffect(() => {
+    console.log("Session data:", session);
+    // @ts-ignore
+    if (session?.accessToken && session.user?.name) {
+      // @ts-ignore
+      analytics(session.accessToken, session.user.name);
+    }
+  }, [session]);
+
+  const analytics = async (accessToken: string, username: string) => {
+    console.log(process.env.TWITCH_CLIENT_ID);
+    try {
+      const response = await fetch(`https://api.twitch.tv/helix/analytics/extensions`, {
+        method: "GET",
+        headers: {
+          "Client-Id": process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID!,
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        console.error("Twitch API error", await response.text());
+        return;
+      }
+
+      const data = await response.json();
+      console.log("Twitch Analytics:", data);
+    } catch (err) {
+      console.error("Fetch failed", err);
+    }
+  };
+
   if (session) {
     return (
       <>
-        <p>Signed in as {session.user?.name}</p>
-        <button onClick={() => signOut()}>Sign out</button>
+        <p className="font-bold text-lg m-2">Signed in as {session.user?.name}</p>
+        <button className="bg-red-600 p-4 m-2 rounded-2xl" onClick={() => signOut()}>
+          <LogOut />
+        </button>
       </>
     );
   }
 
-  return <p>Non sei loggato</p>;
+  return <p className="font-bold text-2xl m-2">Non sei loggato</p>;
 }
--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -6,7 +6,23 @@ export const authOptions: NextAuthOptions = {
     TwitchProvider({
       clientId: process.env.TWITCH_CLIENT_ID!,
       clientSecret: process.env.TWITCH_CLIENT_SECRET!,
+      authorization: { params: { scope: "openid user:read:email moderation:read moderator:manage:banned_users analytics:read:extensions" } },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        console.log("account", account);
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // @ts-ignore
+      session.accessToken = token.accessToken
+
+      return session
+    }
+  },
 };
